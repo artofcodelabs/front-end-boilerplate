f8d6c1695473c60b87c293d57efec47622e3c831
himn1 <zbigniew.humeniuk@gmail.com>
2017-12-28 23:30:50 +0100

    deps were updated

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const postCssOptions = {
   // Necessary for external CSS imports to work
