81e0c2044db6bc875cdd78b9522825d8a20fe5b4
himn1 <zbigniew.humeniuk@gmail.com>
2018-01-03 00:41:39 +0100

    FilterLink is working

--- src/javascripts/containers/page1/VisibleArticleList.js
+++ src/javascripts/containers/page1/VisibleArticleList.js
@@ -3,8 +3,20 @@ import {connect} from 'react-redux';
 import {loadArticles, markAsRead} from 'actions/page1/articles';
 import ArticleList from 'components/page1/ArticleList';
 
+const getReadArticles = (articles, visibilityFilter) => {
+  switch(visibilityFilter){
+    case 'SHOW_READ':
+      return articles.filter(a => a.read)
+    case 'SHOW_UNREAD':
+      return articles.filter(a => !a.read)
+    case 'SHOW_ALL':
+    default:
+      return articles
+  }
+};
+
 const mapStateToProps = state => (
-  { articles: state.articles
+  { articles: getReadArticles(state.articles, state.visibilityFilter)
   }
 );
 
