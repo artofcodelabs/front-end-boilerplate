429e40e3dfb9042f6d66a8e90c7899ec65c03a0c
himn1 <zbigniew.humeniuk@gmail.com>
2018-01-02 23:37:05 +0100

    Refactoring

--- src/javascripts/components/page1/Footer.js
+++ src/javascripts/components/page1/Footer.js
@@ -1,44 +1,41 @@
 import React from 'react';
 import Link from './Link';
 
-class Footer extends React.Component{
-  render(){
-    return(
-      <div>
-        <hr />
-        <p>
-          Show:
+const Footer = () => (
+  <div>
+    <hr />
 
-          {' '}
+    <p>
+      Show:
 
-          <Link
-            active={true}
-            onClick={() => (console.log('all clicked'))}
-          >
-            All
-          </Link>
+      {' '}
 
-          {', '}
+      <Link
+        active={true}
+        onClick={() => (console.log('all clicked'))}
+      >
+        All
+      </Link>
 
-          <Link
-            active={false}
-            onClick={() => (console.log('read clicked'))}
-          >
-            Read
-          </Link>
+      {', '}
 
-          {', '}
+      <Link
+        active={false}
+        onClick={() => (console.log('read clicked'))}
+      >
+        Read
+      </Link>
 
-          <Link
-            active={false}
-            onClick={() => (console.log('unread clicked'))}
-          >
-            Unread
-          </Link>
-        </p>
-      </div>
-    )
-  }
-}
+      {', '}
+
+      <Link
+        active={false}
+        onClick={() => (console.log('unread clicked'))}
+      >
+        Unread
+      </Link>
+    </p>
+  </div>
+);
 
 export default Footer;
