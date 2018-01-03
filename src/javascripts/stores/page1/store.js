0d8dc83b7e96651bcc9935b260d987048249d979
himn1 <zbigniew.humeniuk@gmail.com>
2018-01-04 00:16:08 +0100

    window.devToolsExtension

--- src/javascripts/stores/page1/store.js
+++ src/javascripts/stores/page1/store.js
@@ -1,8 +1,13 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from 'reducers/page1';
 
-const store = createStore(rootReducer, undefined, applyMiddleware(thunk));
+const enhancers = compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const store = createStore(rootReducer, undefined, enhancers);
 
 export default store;
