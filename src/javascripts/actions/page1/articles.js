82594351c8368bc485bf05d866d8a6709f5dc813
himn1 <zbigniew.humeniuk@gmail.com>
2018-01-06 15:25:32 +0100

    Formatting

--- src/javascripts/actions/page1/articles.js
+++ src/javascripts/actions/page1/articles.js
@@ -1,21 +1,19 @@
 import Article from 'models/Article';
 
-export const loadArticles = () => (
-  async (dispatch) => {
-    try {
-      const resp = await Article.all({resource: 'main'});
-      dispatch({
-        type: 'ADD_ARTICLES',
-        articles: resp.resources
-      });
-    } catch(err) {
-      dispatch({
-        type: 'FETCH_ARTICLES_FAILURE',
-        msg: "Something went wrong"
-      });
-    }
+export const loadArticles = () => async (dispatch) => {
+  try {
+    const resp = await Article.all({resource: 'main'});
+    dispatch({
+      type: 'ADD_ARTICLES',
+      articles: resp.resources
+    });
+  } catch(err) {
+    dispatch({
+      type: 'FETCH_ARTICLES_FAILURE',
+      msg: "Something went wrong"
+    });
   }
-);
+};
 
 export const markAsRead = id => ({
   type: 'MARK_AS_READ',
