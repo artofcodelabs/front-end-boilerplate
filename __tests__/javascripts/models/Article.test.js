b0de1f3f631b107f018e090320dfbc8a5f39bcfc
himn1 <zbigniew.humeniuk@gmail.com>
2018-01-10 17:11:53 +0100

    eslint + prettier for tests

--- __tests__/javascripts/models/Article.test.js
+++ __tests__/javascripts/models/Article.test.js
@@ -1,7 +1,7 @@
 import Article from "models/Article";
 
-test('vulgarityLevel', () => {
-  const article = new Article({title: 'fuck this!'});
+test("vulgarityLevel", () => {
+  const article = new Article({ title: "fuck this!" });
   article.isValid();
-  expect(article.errors.base[0]).toEqual('Article contains strong language.');
+  expect(article.errors.base[0]).toEqual("Article contains strong language.");
 });
