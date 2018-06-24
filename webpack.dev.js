cbb3305b872bf8374ec88058c8fba34b8af6b445
Zbigniew Humeniuk <himn1@users.noreply.github.com>
2018-06-25 01:40:22 +0200

    Webpack4
    * webpack 4
    
    * webpack-sources is not needed with webpack 4

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
+  mode: "development",
   devtool: "inline-source-map",
   output: {
     publicPath: "/assets/"
