69c3ac1c70e2a1f724c846231015874312e7f14b
Zbigniew Humeniuk <himn1@users.noreply.github.com>
2018-06-27 21:24:05 +0200

    Deps were updated + imagemin-webpack-plugin instead of image-webpack-loader (#4)

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const webpack = require("webpack");
 const merge = require("webpack-merge");
 const MinifyPlugin = require("babel-minify-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
 /* eslint-enable import/no-extraneous-dependencies */
 const common = require("./webpack.common.js");
 
@@ -29,34 +30,6 @@ module.exports = merge.smart(common, {
       {
         test: /\.(sass|scss)$/,
         use: styleLoaders
-      },
-      {
-        test: /\.(png|svg|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "image-webpack-loader",
-            options: {
-              gifsicle: {
-                interlaced: false
-              },
-              optipng: {
-                optimizationLevel: 7
-              },
-              pngquant: {
-                quality: "65-90",
-                speed: 4
-              },
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              },
-              // Specifying webp here will create a WEBP version of your JPG/PNG images
-              webp: {
-                quality: 75
-              }
-            }
-          }
-        ]
       }
     ]
   },
@@ -66,6 +39,11 @@ module.exports = merge.smart(common, {
       "process.env": {
         NODE_ENV: JSON.stringify("production"),
         BABEL_ENV: JSON.stringify("production")
+      }
+    }),
+    new ImageminPlugin({
+      pngquant: {
+        quality: "95-100"
       }
     })
   ],
