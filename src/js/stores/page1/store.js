a47b6ce8d70118373f99b01e7d0bd98bfba33a4d
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-01-24 17:29:50 +0100

    Deprecation warning has been removed

--- src/js/stores/page1/store.js
+++ src/js/stores/page1/store.js
@@ -4,10 +4,14 @@ import thunk from "redux-thunk";
 import { isProd } from "helpers/common";
 import rootReducer from "reducers/page1";
 
+/* eslint-disable no-underscore-dangle */
 const enhancers = compose(
   applyMiddleware(thunk),
-  !isProd() && window.devToolsExtension ? window.devToolsExtension() : f => f
+  !isProd() && window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
 );
+/* eslint-enable no-underscore-dangle */
 
 const store = createStore(rootReducer, undefined, enhancers);
 
