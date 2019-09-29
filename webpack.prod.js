a84f5575e1ec94e68b7f80711e2ec0c5b71d88dc
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-09-29 22:23:16 +0200

    no babel-minify-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,6 @@
 /* eslint-env node */
 const fs = require("fs");
-const webpack = require("webpack");
 const merge = require("webpack-merge");
-const MinifyPlugin = require("babel-minify-webpack-plugin");
 const ImageminPlugin = require("imagemin-webpack-plugin").default;
 const common = require("./webpack.common.js");
 
@@ -27,13 +25,6 @@ module.exports = merge.smart(common, {
     ]
   },
   plugins: [
-    new MinifyPlugin(),
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production"),
-        BABEL_ENV: JSON.stringify("production")
-      }
-    }),
     new ImageminPlugin({
       pngquant: {
         quality: "95-100"
