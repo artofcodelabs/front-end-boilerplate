566c1d4254050cdc9de0dc1e18afdf120c4cb9a1
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-09-30 00:19:38 +0200

    postcss-* has been updated

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,12 @@
 /* eslint-env node */
 
 module.exports = {
-  // Necessary for external CSS imports to work
-  // https://github.com/facebookincubator/create-react-app/issues/2677
   ident: "postcss",
   sourceMap: true,
   plugins: [
-    require("postcss-flexbugs-fixes"),
-    require("autoprefixer")({
-      flexbox: "no-2009"
-    }),
+    require("autoprefixer"),
     require("postcss-custom-properties"),
+    require("postcss-flexbugs-fixes"),
     require("postcss-nested")
   ]
 };
