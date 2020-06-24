4dee8c92d6809e11e16a69f4aca8e81459d227bc
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-06-24 12:52:47 +0200

    Update webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -65,16 +65,14 @@ module.exports = {
       cacheGroups: {
         commons: {
           chunks: "initial",
+          name: "commons",
           minChunks: 2,
-          maxInitialRequests: 5, // The default limit is too small to showcase the effect
-          minSize: 0 // This is example is too small to create commons chunks
+          minSize: 5000 // The default is too small to create commons chunks
         },
         vendor: {
           test: /node_modules/,
-          chunks: "initial",
-          name: "vendor",
-          priority: 10,
-          enforce: true
+          chunks: "all",
+          name: "vendor"
         }
       }
     }
