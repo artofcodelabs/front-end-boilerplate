36091574298d0cab4247763da4d4808b9e134d32
himn1 <hello@artofcode.co>
2020-09-03 11:03:30 +0200

    update bin/clear.sh

--- bin/clear.sh
+++ bin/clear.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm MIT-LICENSE
+rm README.md
 
 rm __tests__/js/models/*
 rm -r dev/server_responses
@@ -8,25 +9,15 @@ rm -r dev/server_responses
 rm public/cubing.html
 rm public/squaring.html
 
-rm -r src/css/index
-rm -r src/css/shared
-rm src/css/*
-rm src/fonts/*
-rm src/images/*
 rm -r src/js/actions/page1
-rm -r src/js/components/index
+rm src/js/components/index/Calcs.js
 rm -r src/js/components/page1
 rm -r src/js/components/shared
-rm src/js/config/*
 rm -r src/js/containers/page1
-rm src/js/helpers/*
+rm src/js/helpers/math.js
 rm src/js/models/*
 rm -r src/js/reducers/page1
 rm -r src/js/stores/page1
-
-cat dev/router.js \
-  | awk -v regex="squaring" -v count="9" '$0 ~ regex { skip=count; next } --skip >= 0 { next } 1' \
-  | tee dev/router.js
 
 if [ -d ".git" ]; then
   rm -rf .git
