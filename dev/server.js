2f22bbeedd3b6a26f21e38f3db3c98b18bb2d212
himn1 <hello@artofcode.co>
2022-10-17 23:01:23 +0200

    node 16

--- dev/server.js
+++ dev/server.js
@@ -1,13 +1,10 @@
-const rootPath = '..';
-
-const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const Router = require('./router');
 
 const app = express();
-const config = require(`${rootPath}/webpack.dev.js`);
+const config = require('../webpack.dev.js');
 const compiler = webpack(config);
 const router = new Router(app);
 
