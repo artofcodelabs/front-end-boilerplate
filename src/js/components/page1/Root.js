3715e4e4c2962ec73748d78728214d66971df187
Zbig <hello@artofcode.co>
2022-11-17 22:59:29 +0100

    React Router on page1

--- src/js/components/page1/Root.js
+++ src/js/components/page1/Root.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
 import VisibleArticleList from "containers/page1/VisibleArticleList";
 import Assets from "./Assets";
@@ -16,6 +17,11 @@ const Root = ({ showFooter }) => (
           <a href="#" className="active">
             Page 1
           </a>
+          <ul>
+            <li>
+              <NavLink to={`/page1.html/assets`}>Assets</NavLink>
+            </li>
+          </ul>
         </li>
       </ul>
     </nav>
--- src/page1.js
+++ src/page1.js
@@ -2,6 +2,12 @@ import "core-js/stable";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
 import "config";
 import store from "stores/page1/store";
@@ -10,11 +16,17 @@ import App from "containers/page1/App";
 import "shared/index.css";
 import "page1.css";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="page1.html" element={<App />}></Route>)
+);
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <Provider store={store}>
-    <App />
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   </Provider>
 );
 
