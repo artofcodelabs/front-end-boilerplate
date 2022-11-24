5abdec0f90b14134d31e225d5e75a37b5e07e4c0
Zbig <hello@artofcode.co>
2022-11-24 23:33:20 +0100

    remove FetchError

--- src/js/components/page1/ArticleList.js
+++ src/js/components/page1/ArticleList.js
@@ -4,13 +4,16 @@ import { NavLink } from "react-router-dom";
 
 import ArticleModel from "models/Article";
 import Article from "./Article";
-import FetchError from "./FetchError";
 
 const ArticleList = ({ articles, showLink, onMarkAsReadClick, errorMsg }) => (
   <div>
     <h2>Articles</h2>
 
-    {errorMsg && !articles.length ? <FetchError msg={errorMsg} /> : ""}
+    {errorMsg && !articles.length ? (
+      <p style={{ color: "red" }}>{errorMsg}</p>
+    ) : (
+      ""
+    )}
 
     {showLink ? <NavLink to={"/page1/all"}>Load Articles</NavLink> : ""}
 
