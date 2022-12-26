7376bb27fcad352e61f3223b42f3365646d14cc7
Zbig <hello@artofcode.co>
2022-12-26 23:52:07 +0100

    refactoring

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,8 +5,4 @@ const common = require("./webpack.common.js");
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
-  entry: {
-    index: "./src/index",
-    page1: "./src/page1",
-  },
 });
