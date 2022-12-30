c187c7d3530642fe113ed1629eee4c8a495e6bd7
Zbig <hello@artofcode.co>
2022-12-30 23:34:51 +0100

    CSS refactoring

--- src/js/components/shared/NavLink.js
+++ src/js/components/shared/NavLink.js
@@ -5,7 +5,7 @@ import { NavLink as RouterNavLink } from "react-router-dom";
 const NavLink = ({ to, children }) => {
   return (
     <RouterNavLink to={to}>
-      <span className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">
+      <span className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700 active:bg-indigo-800">
         {children}
       </span>
     </RouterNavLink>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.js"],
   theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
