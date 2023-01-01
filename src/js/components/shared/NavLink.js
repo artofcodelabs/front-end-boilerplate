5b8fae2daa89824b87238e083a22de7892ce591f
Zbig <hello@artofcode.co>
2023-01-01 22:31:20 +0100

    active style

--- src/js/components/shared/NavLink.js
+++ src/js/components/shared/NavLink.js
@@ -2,12 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import { NavLink as RouterNavLink } from "react-router-dom";
 
+const spanClass =
+  "inline-block mr-2 rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700 active:bg-indigo-800";
+
 const NavLink = ({ to, children }) => {
   return (
     <RouterNavLink to={to}>
-      <span className="inline-block mr-2 rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700 active:bg-indigo-800">
-        {children}
-      </span>
+      {({ isActive }) => (
+        <span className={isActive ? `${spanClass} bg-indigo-800` : spanClass}>
+          {children}
+        </span>
+      )}
     </RouterNavLink>
   );
 };
