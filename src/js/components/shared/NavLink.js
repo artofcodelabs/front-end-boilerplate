1067fb93b68a066771deb22ccf29dbfc6f473377
Zbig <hello@artofcode.co>
2023-01-07 23:33:40 +0100

    active on demand

--- src/js/components/shared/NavLink.js
+++ src/js/components/shared/NavLink.js
@@ -5,11 +5,15 @@ import { NavLink as RouterNavLink } from "react-router-dom";
 const spanClass =
   "inline-block mr-2 rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700 active:bg-indigo-800";
 
-const NavLink = ({ to, children }) => {
+const NavLink = ({ to, active, children }) => {
   return (
     <RouterNavLink to={to}>
       {({ isActive }) => (
-        <span className={isActive ? `${spanClass} bg-indigo-800` : spanClass}>
+        <span
+          className={
+            isActive || active ? `${spanClass} bg-indigo-800` : spanClass
+          }
+        >
           {children}
         </span>
       )}
@@ -19,6 +23,7 @@ const NavLink = ({ to, children }) => {
 
 NavLink.propTypes = {
   to: PropTypes.string.isRequired,
+  active: PropTypes.bool,
   children: PropTypes.node.isRequired,
 };
 
