673d08fa47e15c28fc90cd9b73e1b2e8ac1f57f2
Zbig <hello@artofcode.co>
2023-01-15 22:10:06 +0100

    fix tests

--- __tests__/js/e2e/page1Test.js
+++ __tests__/js/e2e/page1Test.js
@@ -1,6 +1,6 @@
 import { Selector } from "testcafe";
 
-fixture`Page 1`.page`http://localhost:3000/page1`;
+fixture`Page 1`.page`http://localhost:3000/page1/articles`;
 
 test("loading articles", async (t) => {
   await t
